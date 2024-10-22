c18bea1b7d029344cc3314eb61347666111921bd
zkyntu <kunyangzhou@seu.edu.cn>
2024-10-22 14:54:07 +0800

    Update scnn.py

--- unlanedet/model/SCNN/scnn.py
+++ unlanedet/model/SCNN/scnn.py
@@ -1,103 +1,32 @@
-import math
-import torch
 from torch import nn
 import torch.nn.functional as F
 from torch.hub import load_state_dict_from_url
 from ..module.architecture import Detector
 
-def is_tracing() -> bool:
-    # https://github.com/pytorch/pytorch/issues/42448
-    if torch.__version__ >= '1.7.0':
-        return torch.jit.is_tracing()
-    elif torch.__version__ >= '1.6.0':
-        return torch._C._is_tracing()
-    else:
-        return False
 
 class SCNN_AGG(nn.Module):
-    def __init__(self, num_channels=128):
-        super().__init__()
-        self.conv_d = nn.Conv2d(num_channels, num_channels, (1, 9), padding=(0, 4))
-        self.conv_u = nn.Conv2d(num_channels, num_channels, (1, 9), padding=(0, 4))
-        self.conv_r = nn.Conv2d(num_channels, num_channels, (9, 1), padding=(4, 0))
-        self.conv_l = nn.Conv2d(num_channels, num_channels, (9, 1), padding=(4, 0))
-        self._adjust_initializations(num_channels=num_channels)
+    def __init__(self, cfg=None):
+        super(SCNN, self).__init__()
+        self.conv_d = nn.Conv2d(128, 128, (1, 9), padding=(0, 4), bias=False)
+        self.conv_u = nn.Conv2d(128, 128, (1, 9), padding=(0, 4), bias=False)
+        self.conv_r = nn.Conv2d(128, 128, (9, 1), padding=(4, 0), bias=False)
+        self.conv_l = nn.Conv2d(128, 128, (9, 1), padding=(4, 0), bias=False)
 
-    def _adjust_initializations(self, num_channels=128):
-        # https://github.com/XingangPan/SCNN/issues/82
-        bound = math.sqrt(2.0 / (num_channels * 9 * 5))
-        nn.init.uniform_(self.conv_d.weight, -bound, bound)
-        nn.init.uniform_(self.conv_u.weight, -bound, bound)
-        nn.init.uniform_(self.conv_r.weight, -bound, bound)
-        nn.init.uniform_(self.conv_l.weight, -bound, bound)
+    def forward(self, x):
+        x = x.clone()
+        for i in range(1, x.shape[2]):
+            x[..., i:i+1, :] = x[..., i:i+1, :].clone() + F.relu(self.conv_d(x[..., i-1:i, :].clone()))
 
-    def build_slice(self, vertical, reverse, shape):
-        # refer to https://github.com/harryhan618/SCNN_Pytorch/blob/master/model.py
-        slices = []
-        if vertical: # h
-            concat_dim = 2 
-            length = shape[concat_dim]
-            for i in range(length):
-                slices.append(
-                    (slice(None), slice(None), slice(i, i+1), slice(None))
-                )
-        else: # w
-            concat_dim = 3
-            length = shape[concat_dim]
-            for i in range(length):
-                slices.append(
-                    (slice(None), slice(None), slice(None), slice(i, i+1))
-                )
-        if reverse:
-            slices = slices[::-1]
-        return slices, concat_dim
+        for i in range(x.shape[2] - 2, 0, -1):
+            x[..., i:i+1, :] = x[..., i:i+1, :].clone() + F.relu(self.conv_u(x[..., i+1:i+2, :].clone()))
 
-    def non_inplace_forward(self, input):
-        output = input
-        vertical = [True, True, False, False]
-        reverse = [False, True, False, True]
-        convs = [self.conv_d, self.conv_u, self.conv_r, self.conv_l]
-        for ver, rev, conv in zip(vertical, reverse, convs):
-            slices, dim = self.build_slice(ver, rev, input.shape)
-            output_slices = []
-            for idx, s in enumerate(slices):
-                # the condition is to align with the original forward
-                if (rev is False and idx > 0) or (rev is True and idx < len(slices) - 1 and idx > 0):
-                    output_slices.append(
-                        output[s]  + F.relu(conv(output_slices[-1]))
-                    )
-                else:
-                    output_slices.append(output[s])
-            if rev:
-                output_slices = output_slices[::-1]
-            output = torch.cat(output_slices, dim=dim)
-            if ver is False and rev is True:
-                break
-        return output
+        for i in range(1, x.shape[3]):
+            x[..., i:i+1] = x[..., i:i+1].clone() + F.relu(self.conv_r(x[..., i-1:i].clone()))
 
-    def forward(self, input):
-        output = input
-
-        if is_tracing():
-            # PyTorch index+add_ will be ignored in traced graph
-            # Down
-            for i in range(1, output.shape[2]):
-                output[:, :, i:i + 1, :] = output[:, :, i:i + 1, :].add(F.relu(self.conv_d(output[:, :, i - 1:i, :])))
-            # Up
-            for i in range(output.shape[2] - 2, 0, -1):
-                output[:, :, i:i + 1, :] = output[:, :, i:i + 1, :].add(
-                    F.relu(self.conv_u(output[:, :, i + 1:i + 2, :])))
-            # Right
-            for i in range(1, output.shape[3]):
-                output[:, :, :, i:i + 1] = output[:, :, :, i:i + 1].add(F.relu(self.conv_r(output[:, :, :, i - 1:i])))
-            # Left
-            for i in range(output.shape[3] - 2, 0, -1):
-                output[:, :, :, i:i + 1] = output[:, :, :, i:i + 1].add(
-                    F.relu(self.conv_l(output[:, :, :, i + 1:i + 2])))
-        else:
-            output = self.non_inplace_forward(output)
-
-        return output
+        for i in range(x.shape[3] - 2, 0, -1):
+            x[..., i:i+1] = x[..., i:i+1].clone() + F.relu(self.conv_l(x[..., i+1:i+2].clone()))
+            # x[..., i:i+1].add_(F.relu(self.conv_l(x[..., i+1:i+2])))
+        return x
 
 class SCNN(Detector):
     def __init__(self, backbone=None, aggregator=None, neck=None, head=None):
