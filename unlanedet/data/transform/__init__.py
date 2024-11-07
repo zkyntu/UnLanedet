9224bc1724e7825428bbf5757f6034293cb25979
zkyntu <kunyangzhou@seu.edu.cn>
2024-11-07 21:44:33 +0800

    Update __init__.py

--- unlanedet/data/transform/__init__.py
+++ unlanedet/data/transform/__init__.py
@@ -1,6 +1,6 @@
 from .alaug import Alaug
 from .collect_lane import CollectLane
 from .generate_lane_cls import GenerateLaneCls
-from .generate_lane_line import GenerateLaneLine
+from .generate_lane_line import GenerateLaneLine,GenerateLaneLineATT
 from .transforms import *
 from .datacontainer import DataContainer
